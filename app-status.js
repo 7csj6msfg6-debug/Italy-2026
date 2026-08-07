e2f88e5867fe50eebf8f17a100095f1005552c9e
7csj6msfg6-debug <7csj6msfg6@privaterelay.appleid.com>
2026-08-07 14:41:21 -0400

    Bump app version for Today map fix

--- app-status.js
+++ app-status.js
@@ -1,6 +1,6 @@
 (() => {
-  const BUILD = '2026.08.07.4';
-  const EXPECTED_CACHE = 'italy-2026-app-v9';
+  const BUILD = '2026.08.07.5';
+  const EXPECTED_CACHE = 'italy-2026-app-v10';
 
   function ensureStyles() {
     if (document.getElementById('app-status-styles')) return;
